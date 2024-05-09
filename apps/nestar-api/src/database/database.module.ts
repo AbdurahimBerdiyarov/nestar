fd0e8666d4f6fb2a0336a6dfcab4a08d475f28d4
AbdurahimBerdiyarov <hoorwen@icloud.com>
2024-05-09 23:01:20 +0900

    fix: connection MongoDB database

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -1,4 +1,76 @@
-import { Module } from '@nestjs/common';
+// import { Module } from '@nestjs/common';
+// import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+// import { Connection } from 'mongoose';
 
-@Module({})
-export class DatabaseModule {}
+// @Module({
+// 	imports: [
+// 		MongooseModule.forRootAsync({
+// 			useFactory: () => ({
+// 				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+// 			}),
+// 		}),
+// 	],
+// 	exports: [MongooseModule],
+// })
+// export class DatabaseModule {
+// 	constructor(@InjectConnection() private readonly connection: Connection) {
+// 		if (connection.readyState === 1) {
+// 			console.log(
+// 				`MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`,
+// 			);
+// 		} else {
+// 			console.log('DB is not connected');
+// 		}
+// 	}
+// }
+
+// import { Module } from '@nestjs/common';
+// import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+// import { Connection } from 'mongoose';
+
+// @Module({
+// 	imports: [
+// 		MongooseModule.forRootAsync({
+// 			useFactory: () => ({
+// 				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+// 			}),
+// 		}),
+// 	],
+// 	exports: [MongooseModule],
+// })
+// export class DatabaseModule {
+// 	constructor(@InjectConnection() private readonly connection: Connection) {
+// 		if (connection.readyState === 1) {
+// 			console.log(
+// 				`MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`,
+// 			);
+// 		} else {
+// 			console.log('DB is not connected');
+// 		}
+// 	}
+// }
+import { Module } from '@nestjs/common';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Module({
+	imports: [
+		MongooseModule.forRootAsync({
+			useFactory: () => ({
+				uri: process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+			}),
+		}),
+	],
+	exports: [MongooseModule],
+})
+export class DatabaseModule {
+	constructor(@InjectConnection() private readonly connection: Connection) {
+		if (connection.readyState === 1) {
+			console.log(
+				`MongoDB is connected to ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} mode`,
+			);
+		} else {
+			console.log('DB is not connected!');
+		}
+	}
+}
