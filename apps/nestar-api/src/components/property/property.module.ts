5d081cb72782680a210154c394abc08859a2001c
AbdurahimBerdiyarov <hoorwen@icloud.com>
2024-05-29 14:08:18 +0900

    feat: develop likeTargetProperty GraphQL api

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -8,6 +8,7 @@ import { ViewModule } from '../view/view.module';
 // import { MemberModule } from '../member/member.module';
 // import { MemberService } from '../member/member.service';
 import { MemberModule } from '../member/member.module';
+import { LikeModule } from '../like/like.module';
 
 @Module({
 	imports: [
@@ -15,6 +16,7 @@ import { MemberModule } from '../member/member.module';
 		AuthModule,
 		ViewModule,
 		MemberModule,
+		LikeModule,
 	],
 	providers: [PropertyResolver, PropertyService],
 	exports: [PropertyService],
