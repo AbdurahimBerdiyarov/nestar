b74b0344455747c1f691786bac7164f46d0f245d
AbdurahimBerdiyarov <hoorwen@icloud.com>
2024-08-26 22:21:40 +0900

    feat: deploy nestar monorepo project on docker containers

--- apps/homezone-batch/src/main.ts
+++ apps/homezone-batch/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { BatchModule } from './batch.module';
+import { HomeZoneBatchModule } from './batch.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(BatchModule);
+	const app = await NestFactory.create(HomeZoneBatchModule);
 	await app.listen(process.env.PORT_BATCH ?? 4000);
 }
 bootstrap();
